e052ca7dab375db45fe13df23cc9699ad08f7432
hassanzohdy <hassanzohdy@gmail.com>
2021-03-10 13:03:16 +0200

    Fixed Base Url Name

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -7,7 +7,7 @@ export const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 const settings: ConfigList = {
     langMode: 'array',
     endpoint: {
-        baseUrl: process.env.REACT_APP_WIZARD_BASE_URL,
+        baseUrl: process.env.REACT_APP_API_BASE_URL,
         apiKey: process.env.REACT_APP_API_KEY,
     },
     locales: {
